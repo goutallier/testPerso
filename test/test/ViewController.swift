89ad6f6d90ef6ea776fba53dae77bebdebc3e99e
Geoffrey Goutallier <geoffrey@goutallier.net>
2021-11-10 10:27:59 +0100

    create xcode project...

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Geoffrey Goutallier on 10/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
